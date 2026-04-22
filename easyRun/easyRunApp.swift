214f8fcb9da9b8c1938b7086435139a18e9d991c
jimmy <tliang92@gmail.com>
2026-04-22 10:59:41 +0800

    Initial Commit

---
+++ easyRun/easyRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  easyRunApp.swift
+//  easyRun
+//
+//  Created by JimmyL on 2026/4/22.
+//
+
+import SwiftUI
+
+@main
+struct easyRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
